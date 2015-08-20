90e3f0ddcf52ac1da56bddfb4c7f320f3c1fcf83
taras.oleynik <taras.oleynik@gmail.com>
2015-08-20 18:11:20 +0300

    removing

--- src/main/java/MyFirstProgram.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Taras_Oliinyk on 8/20/2015.
- */
-public class MyFirstProgram {
-    public static void main(String args[]){
-
-        System.out.println("Hello World");
-    }
-}
